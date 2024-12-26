d7b8e99d65761d4101b1794af3f52b31bb303888
Cyril-Nii <cyriltagoe0@gmail.com>
2024-12-26 02:47:15 +0000

    Implemented some methods and added a few properties

--- src/main/java/com/vms_sample/Car.java
+++ src/main/java/com/vms_sample/Car.java
@@ -3,38 +3,107 @@ package com.vms_sample;
 public class Car extends Vehicle{
 
         private int numberOfSeats;
+        private boolean hasAC;
+        private boolean hasLeatherSeats;
+        private boolean hasGPS;
+        private double baseRentalRate = 120.00;
+
 
         // Constructor
-        public Car(String vehicleId, String model, double baseRentalRate, boolean isAvailable, int numberOfSeats) {
-            super(vehicleId, model, baseRentalRate, isAvailable);
+        public Car(String vehicleId, String manufacturer, String model, double baseRentalRate, boolean isAvailable, String color, int numberOfSeats, boolean hasAC, boolean hasLeatherSeats, boolean hasGPS) {
+            super(vehicleId, manufacturer, model, baseRentalRate, isAvailable, color);
             this.numberOfSeats = numberOfSeats;
+            this.hasAC = hasAC;
+            this.hasLeatherSeats = hasLeatherSeats;
+            this.hasGPS = hasGPS;
         }
-
 
         // Getters and Setters
         public int getNumberOfSeats() {
             return numberOfSeats;
         }
 
-        public void setNumberOfSeats(int numberOfSeats) {
-            this.numberOfSeats = numberOfSeats;
+        public boolean setNumberOfSeats(int numberOfSeats) {
+            if (numberOfSeats <= 0) {
+                return false;
+            }
+
+            else {
+                this.numberOfSeats = numberOfSeats;
+                return true;
+            }
+        }
+
+        public boolean getHasAC() {
+            return hasAC;
+        }
+
+        public boolean setHasAC(boolean hasAC) {
+            this.hasAC = hasAC;
+            return true;
+        }
+
+        public boolean getHasLeatherSeats() {
+            return hasLeatherSeats;
+        }
+
+        public boolean setHasLeatherSeats(boolean hasLeatherSeats) {
+            this.hasLeatherSeats = hasLeatherSeats;
+            return true;
+        }
+
+        public boolean getHasGPS() {
+            return hasGPS;
+        }
+
+        public boolean setHasGPS(boolean hasGPS) {
+            this.hasGPS = hasGPS;
+            return true;
         }
 
 
 
         @Override
         public double calculateRentalCost(int days) {
+            if (days <= 0) {
+                return 0.0;
+            }
 
-            double rentalCost = getBaseRentalRate() * days;
+            else {
+                double rentalRate = getBaseRentalRate();
 
-            System.out.println("The rental cost for " + getModel() + " for " + days + " is $" + rentalCost);
-            return rentalCost;
+                // Add additional charges based on the car's features
+                if (numberOfSeats > 4) {
+                    rentalRate += 50.00;
+                }
+
+                if (hasAC) {
+                    rentalRate += 40.00;
+                }
+
+                if (hasLeatherSeats) {
+                    rentalRate += 30.00;
+                }
+
+                if (hasGPS) {
+                    rentalRate += 20.00;
+                }
+
+                double rentalCost = rentalRate * days;
+
+                return rentalCost;
+            }
         }
-
 
         @Override
         public boolean isAvailableForRental() {
-            return getIsAvailable();
+            //if(){
+
+            //}
+
+            //else {
+                return getIsAvailable();
+            //}
         }
 
         @Override
@@ -46,6 +115,34 @@ public class Car extends Vehicle{
                     "\nBase Rental Rate = " + getBaseRentalRate() +
                     "\nIs Available = " + getIsAvailable() + "\n";
         }
+
+        // Override equals() and hashCode() methods
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Car car = (Car) obj;
+
+            return getVehicleId().equals(car.getVehicleId());
+        }
+
+        @Override
+        public int hashCode() {
+            return getVehicleId().hashCode();
+        }
+
+
+
+
+
+
+
 
 
 }
