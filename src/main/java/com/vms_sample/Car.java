e7cd2f921df8fa745059e28cb319524827358bec
Cyril-Nii <cyriltagoe0@gmail.com>
2024-12-29 03:03:55 +0000

    Modify the Availability method

--- src/main/java/com/vms_sample/Car.java
+++ src/main/java/com/vms_sample/Car.java
@@ -1,148 +1,143 @@
 package com.vms_sample;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle implements Rentable {
 
-        private int numberOfSeats;
-        private boolean hasAC;
-        private boolean hasLeatherSeats;
-        private boolean hasGPS;
-        private double baseRentalRate = 120.00;
+    private int numberOfSeats;
+    private boolean hasAC;
+    private boolean hasLeatherSeats;
+    private boolean hasGPS;
+    private final static double baseRentalRate = 120.00;
 
+    // Constructor
+    public Car(String vehicleId, String manufacturer, String model, double baseRentalRate, boolean isAvailable, String color, int horsepower, TransmissionType transmissionType, int numberOfSeats, boolean hasAC, boolean hasLeatherSeats, boolean hasGPS) {
+        super(vehicleId, manufacturer, model, baseRentalRate, isAvailable, color, horsepower, transmissionType);
 
-        // Constructor
-        public Car(String vehicleId, String manufacturer, String model, double baseRentalRate, boolean isAvailable, String color, int numberOfSeats, boolean hasAC, boolean hasLeatherSeats, boolean hasGPS) {
-            super(vehicleId, manufacturer, model, baseRentalRate, isAvailable, color);
+        // Validate number of seats
+        if (numberOfSeats <= 0) {
+            throw new IllegalArgumentException("Number of seats must be greater than 0");
+        }
+
+        this.numberOfSeats = numberOfSeats;
+        this.hasAC = hasAC;
+        this.hasLeatherSeats = hasLeatherSeats;
+        this.hasGPS = hasGPS;
+    }
+
+    // Getters and Setters
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public boolean setNumberOfSeats(int numberOfSeats) {
+        if (numberOfSeats <= 0) {
+            return false;
+        } else {
             this.numberOfSeats = numberOfSeats;
-            this.hasAC = hasAC;
-            this.hasLeatherSeats = hasLeatherSeats;
-            this.hasGPS = hasGPS;
+            return true;
         }
+    }
 
-        // Getters and Setters
-        public int getNumberOfSeats() {
-            return numberOfSeats;
-        }
+    public boolean getHasAC() {
+        return hasAC;
+    }
 
-        public boolean setNumberOfSeats(int numberOfSeats) {
-            if (numberOfSeats <= 0) {
-                return false;
+    public void setHasAC(boolean hasAC) {
+        this.hasAC = hasAC;
+    }
+
+    public boolean getHasLeatherSeats() {
+        return hasLeatherSeats;
+    }
+
+    public void setHasLeatherSeats(boolean hasLeatherSeats) {
+        this.hasLeatherSeats = hasLeatherSeats;
+    }
+
+    public boolean getHasGPS() {
+        return hasGPS;
+    }
+
+    public void setHasGPS(boolean hasGPS) {
+        this.hasGPS = hasGPS;
+    }
+
+
+
+    @Override
+    public boolean isAvailableForRental() {
+        return isAvailable();
+    }
+
+
+    @Override
+    public double calculateRentalCost(int days) {
+        if (days <= 0) {
+            return 0.0;
+        } else {
+            double rentalRate = getBaseRentalRate();
+
+            // Add additional charges based on the car's features
+            if (numberOfSeats > 4) {
+                rentalRate += 50.00;
             }
 
-            else {
-                this.numberOfSeats = numberOfSeats;
-                return true;
+            if (hasAC) {
+                rentalRate += 40.00;
             }
-        }
 
-        public boolean getHasAC() {
-            return hasAC;
-        }
+            if (hasLeatherSeats) {
+                rentalRate += 30.00;
+            }
 
-        public boolean setHasAC(boolean hasAC) {
-            this.hasAC = hasAC;
+            if (hasGPS) {
+                rentalRate += 20.00;
+            }
+
+            return rentalRate * days;
+        }
+    }
+
+    @Override
+    public boolean rent(Customer customer, int days){
+        if (isAvailableForRental()){
+            setIsAvailable(false);
             return true;
         }
 
-        public boolean getHasLeatherSeats() {
-            return hasLeatherSeats;
-        }
+        return false;
 
-        public boolean setHasLeatherSeats(boolean hasLeatherSeats) {
-            this.hasLeatherSeats = hasLeatherSeats;
+    }
+
+    @Override
+    public boolean returnVehicle() {
+        setIsAvailable(true);
+        return true;
+    }
+
+
+
+
+
+    //Override the to string method
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
         }
 
-        public boolean getHasGPS() {
-            return hasGPS;
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
         }
 
-        public boolean setHasGPS(boolean hasGPS) {
-            this.hasGPS = hasGPS;
-            return true;
-        }
+        Car car = (Car) obj;
 
+        return getVehicleId().equals(car.getVehicleId());
+    }
 
-
-        @Override
-        public double calculateRentalCost(int days) {
-            if (days <= 0) {
-                return 0.0;
-            }
-
-            else {
-                double rentalRate = getBaseRentalRate();
-
-                // Add additional charges based on the car's features
-                if (numberOfSeats > 4) {
-                    rentalRate += 50.00;
-                }
-
-                if (hasAC) {
-                    rentalRate += 40.00;
-                }
-
-                if (hasLeatherSeats) {
-                    rentalRate += 30.00;
-                }
-
-                if (hasGPS) {
-                    rentalRate += 20.00;
-                }
-
-                double rentalCost = rentalRate * days;
-
-                return rentalCost;
-            }
-        }
-
-        @Override
-        public boolean isAvailableForRental() {
-            //if(){
-
-            //}
-
-            //else {
-                return getIsAvailable();
-            //}
-        }
-
-        @Override
-        public String toString() {
-            return "--Car--\n" +
-                    "Model = " + getModel() +
-                    "\nCar Number = " + getVehicleId() +
-                    "\nNumber Of Seats = " + numberOfSeats +
-                    "\nBase Rental Rate = " + getBaseRentalRate() +
-                    "\nIs Available = " + getIsAvailable() + "\n";
-        }
-
-        // Override equals() and hashCode() methods
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-
-            if (obj == null || getClass() != obj.getClass()) {
-                return false;
-            }
-
-            Car car = (Car) obj;
-
-            return getVehicleId().equals(car.getVehicleId());
-        }
-
-        @Override
-        public int hashCode() {
-            return getVehicleId().hashCode();
-        }
-
-
-
-
-
-
-
-
-
+    @Override
+    public int hashCode() {
+        return getVehicleId().hashCode();
+    }
 }
