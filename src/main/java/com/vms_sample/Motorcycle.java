bc210d7529d7450a5badb56b2d06aa4131401d0e
Cyril-Nii <cyriltagoe0@gmail.com>
2024-12-29 03:10:11 +0000

    Add Motorcycle class with rental functionality and features

---
+++ src/main/java/com/vms_sample/Motorcycle.java
@@ -0,0 +1,118 @@
+package com.vms_sample;
+
+public class Motorcycle extends Vehicle implements Rentable {
+
+    private boolean hasSidecar;
+    private boolean isOffroad;
+    private final double baseRentalRate = 80.00;
+
+    // Constructor
+    public Motorcycle(String vehicleId, String manufacturer, String model, double baseRentalRate, boolean isAvailable, String color, int horsepower, TransmissionType transmissionType, boolean hasSidecar, boolean isOffroad) {
+        super(vehicleId, manufacturer, model, baseRentalRate, isAvailable, color, horsepower, transmissionType);
+
+        this.hasSidecar = hasSidecar;
+        this.isOffroad = isOffroad;
+    }
+
+    // Getters and Setters
+    public boolean getHasSidecar() {
+        return hasSidecar;
+    }
+
+    public void setHasSidecar(boolean hasSidecar) {
+        this.hasSidecar = hasSidecar;
+    }
+
+    public boolean getIsOffroad() {
+        return isOffroad;
+    }
+
+    public void setIsOffroad(boolean isOffroad) {
+        this.isOffroad = isOffroad;
+    }
+
+
+
+    @Override
+    public double calculateRentalCost(int days) {
+        if (days <= 0) {
+            return 0.0;
+        } else {
+            double rentalRate = baseRentalRate;
+
+            // Add additional charges based on the motorcycle's features
+            if (hasSidecar) {
+                rentalRate += 20.00;
+            }
+
+            if (isOffroad) {
+                rentalRate += 30.00;
+            }
+
+            return rentalRate * days;
+        }
+    }
+
+
+    @Override
+    public boolean isAvailableForRental() {
+        return isAvailable();
+    }
+
+
+    @Override
+    public boolean rent(Customer customer, int days) {
+        if (isAvailableForRental()) {
+            setIsAvailable(false);
+            // Additional logic for renting the motorcycle
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean returnVehicle() {
+        setIsAvailable(true);
+        // Additional logic for returning the motorcycle
+        return true;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "--Motorcycle--\n" +
+                "Model = " + getModel() +
+                "\nMotorcycle Number = " + getVehicleId() +
+                "\nHas Sidecar = " + hasSidecar +
+                "\nIs Offroad = " + isOffroad +
+                "\nBase Rental Rate = " + getBaseRentalRate() +
+                "\nIs Available = " + isAvailable() + "\n";
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Motorcycle motorcycle = (Motorcycle) obj;
+
+        return getVehicleId().equals(motorcycle.getVehicleId());
+    }
+
+
+    @Override
+    public int hashCode() {
+        return getVehicleId().hashCode();
+    }
+
+
+
+}
